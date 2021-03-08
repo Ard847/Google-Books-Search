e840559d4f0e9e63d50e0848121b881e0d4e150d
ard847 <renatopombal7@gmail.com>
2021-03-08 06:10:51 +0000

    finilised

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const book = require("./model")
 const app = express();
 const cors = require('cors');
+const path = require("path")
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
