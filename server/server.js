9af76d290f28f31cf19e25fe37387562ef6deb7f
ard847 <renatopombal7@gmail.com>
2021-03-08 06:15:33 +0000

    finilised

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
- 
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 /* const connect = async() => {
